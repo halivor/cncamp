17244a7979175de70a349ae5cea341f71a244895
halivor <hongliang186@aliyun.com>
2022-11-02 09:28:55 +0000

    优化dockerfile命令位置

--- module3/build.sh
+++ module3/build.sh
@@ -7,4 +7,4 @@ docker run -d cncamp/httpserver:v1.0.0
 rm -rf ./module2
 PID=`ps -ef | grep "exec" | grep server | grep -v grep | awk '{print $2}'`
 echo $PID
-nsenter -t $PID -n ip addr
+sudo nsenter -t $PID -n ip addr
